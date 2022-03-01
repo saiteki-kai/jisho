4634e4eee355ff5c8d660f12dc4c8584b43cc331
saiteki-kai <giuseppe.magazu@gmail.com>
2022-03-01 21:56:38 +0100

    Change font

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jisho/cubit/word_cubit.dart';
 import 'package:jisho/data/repositories/word.dart';
 import 'package:jisho/pages/home_page.dart';
 
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('assets/fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(const MyApp());
 }
 
@@ -19,6 +26,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.red,
+        fontFamily: "NotoSansJP",
       ),
       home: BlocProvider(
         create: (context) => WordCubit(repository: const WordRepository()),
